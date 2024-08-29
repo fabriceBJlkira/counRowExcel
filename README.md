814461c416e33f53239cb478aab5815760968b1f
Fabrice <fabricej035@gmail.com>
2024-08-29 09:28:49 +0100

    count excel row

---
+++ README.md
@@ -0,0 +1,10 @@
+to run this file, you need php and composer
+how to run ?
+
+you need to install the library usign this comand :
+composer require phpoffice/phpspreadsheet
+
+once it done, open your project on terminal and use this comand to start the server :
+php -S 127.0.0.1:8000
+
+enjoy :) 
---
+++ countRow.php
@@ -0,0 +1,39 @@
+<?php
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+/**
+ * jereny sao de tsy excel le fichier na tsy miexiste
+ */
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['excelFile'])) {
+    $file = $_FILES['excelFile'];
+
+    // jereny loa sao de misy ereur ilay zavatra natao upload
+    if ($file['error'] !== UPLOAD_ERR_OK) {
+        die("Upload failed with error code " . $file['error']);
+    }
+
+    // afindra anaty le dossier uploads le fichier loany
+    $filePath = 'uploads/' . basename($file['name']);
+    move_uploaded_file($file['tmp_name'], $filePath);
+
+    // manao chargement an ilay fichier natao upload anaty dossier uploads 
+    $spreadsheet = IOFactory::load($filePath);
+
+    // maka an ilay fichier amzay
+    $worksheet = $spreadsheet->getActiveSheet();
+
+    // maka total an le ligne amzay
+    $highestRow = $worksheet->getHighestRow();
+
+    // avoka ny resultat
+    echo "Total number of rows: " . $highestRow;
+    echo "<br><a href='/'>Retour en ariere</a>";
+
+    // fafana anaty dosssier uploads ny fichier, ra tsy tiana fafana de afaka esorina ito
+    unlink($filePath);
+} else {
+    echo "No file uploaded or invalid request.";
+}
+
